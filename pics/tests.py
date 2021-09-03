ede4d9df4e516b2ac39bc004a4736b5c22541f18
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-03 16:40:59 +0300

    Created test for category class

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category,Location
+
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method to run tests before
+    def setUp(self):
+        self.food = Category(category_name = 'Food')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food,Category))
+    
