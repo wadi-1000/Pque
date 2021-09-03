13ba1d074da35a73da6dbe6b16f4073fb5e56acf
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-03 17:32:24 +0300

    Added Image Class to admin site

--- pics/admin.py
+++ pics/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Category,Location
+from .models import Category,Location,Image
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Location')
+
+
 admin.site.register(Category)
-admin.site.register(Loaction)
+admin.site.register(Location)
+admin.site.register(Image)
